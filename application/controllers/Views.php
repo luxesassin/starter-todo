d8fc6bb9653fe4b6e162af88e46f5180df271139
Jordan Lin <jordan.lin09@gmail.com>
2017-03-13 00:53:10 -0700

    added task completion features. Closes #17

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -25,9 +25,9 @@ class Views extends Application
             $this->data['leftside'] = $this->makePrioritizedPanel($tasks);
             $this->data['rightside'] = $this->makeCategorizedPanel($tasks);
 
-            $this->render('template_secondary'); 
+            $this->render('template_secondary');
         }
-        
+
         function makePrioritizedPanel($tasks) {
             // extract the undone tasks
             foreach ($tasks as $task)
@@ -37,26 +37,49 @@ class Views extends Application
             }
             // order them by priority
             usort($undone, 'orderByPriority');
-            
+
             // substitute the priority name
             foreach ($undone as $task){
                     $task->priority = $this->priorities->get($task->priority)->name;
             }
-            // convert the array of task objects into an array of associative objects       
+            // convert the array of task objects into an array of associative objects
             foreach ($undone as $task)
                 $converted[] = (array) $task;
-            
-            
+
+
             // and then pass them on
             $parms = ['display_tasks' => $converted];
+
+			$role = $this->session->userdata('userrole');
+			$parms['completer'] = ($role == ROLE_OWNER) ? '/views/complete' : '#';
+			return $this->parser->parse('by_priority', $parms, true);
+
             return $this->parser->parse('by_priority', $parms, true);
         }
-        
+
         function makeCategorizedPanel($tasks)
         {
             $parms = ['display_tasks' => $this->tasks->getCategorizedTasks()];
             return $this->parser->parse('by_category', $parms, true);
         }
+
+		// complete flagged items
+		function complete() {
+		    $role = $this->session->userdata('userrole');
+		    if ($role != ROLE_OWNER) redirect('/work');
+
+		    // loop over the post fields, looking for flagged tasks
+		    foreach($this->input->post() as $key=>$value) {
+		        if (substr($key,0,4) == 'task') {
+		            // find the associated task
+		            $taskid = substr($key,4);
+		            $task = $this->tasks->get($taskid);
+		            $task->status = 2; // complete
+		            $this->tasks->update($task);
+		        }
+		    }
+		    $this->index();
+		}
 
 }
 // return -1, 0, or 1 of $a's priority is higher, equal to, or lower than $b's
